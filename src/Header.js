904aa5e6525750ee0cbe0938c32d2142efe38e25
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-08-29 23:48:08 +0700

    Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import "./main.css";
 import React from "react";
-import tglogo from "./public/img/tglogo.png";
+
 
 function Header() {
   return (
@@ -9,7 +9,6 @@ function Header() {
         <p className="usercard">username</p>
         <a href="https://t.me/zipperwear" className="btnurl">
           <div className="button-telegram-channel">
-            <img src={tglogo} className="tglogo" />
             <p className="widget-text">zipperwear</p>
             <p className="widget-text-low"> наш телеграм канал</p>
           </div>
